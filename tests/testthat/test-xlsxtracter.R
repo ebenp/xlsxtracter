7aeefd91256f377cbfa4c9c5ad8cb6e3adf67068
ebenp <4080051+ebenp@users.noreply.github.com>
2021-03-21 11:04:08 -0400

    Added readme and removed for loop usage.

--- tests/testthat/test-xlsxtracter.R
+++ tests/testthat/test-xlsxtracter.R
@@ -2,17 +2,9 @@
 
 infile <- "test1.xlsx"
 sheet <- "Sheet1"
-startRow <- 3
+startRow <- 4
 
-# cbind example
-lcolrow = list(c("col1","A", 4, 15, "all", startRow, "cbind"),
-               c("col2","B", 4, 15, "all", startRow, "cbind"),
-               c("col3","C", 4, 15, "all", startRow, "cbind"))
-# rbind example
-lcolrow = list(c("col1","A", 4, 6, "all", startRow, "rbind"),
-               c("col1","A", 7, 10, "all", startRow, "rbind"),
-               c("col1","C", 11, 15, "all", startRow, "rbind"))
+colrow = list(c("col1", "col2","col3"), c("A", "B","C"), 4, 15, startRow, 1)
 
 # returns a dataframe
-d <- xlsxtractor(infile, sheet, lcolrow)
-
+d <- xlsxtractor(infile, colrow)
