a03802b177067e5cf5535df50feb52dc586bea56
ebenp <4080051+ebenp@users.noreply.github.com>
2021-03-21 11:12:21 -0400

    Removed startrow as additional parameter

--- tests/testthat/test-xlsxtracter.R
+++ tests/testthat/test-xlsxtracter.R
@@ -4,7 +4,7 @@ infile <- "test1.xlsx"
 sheet <- "Sheet1"
 startRow <- 4
 
-colrow = list(c("col1", "col2","col3"), c("A", "B","C"), 4, 15, startRow, 1)
+colrow = list(c("col1", "col2","col3"), c("A", "B","C"), 4, 15, 1)
 
 # returns a dataframe
 d <- xlsxtractor(infile, colrow)
